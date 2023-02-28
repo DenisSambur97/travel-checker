8624f094c57a4e0c48aef8f45d8e493d0834dcc2
Denis <denissand1997@gmail.com>
2023-02-28 14:45:32 +0300

    Search city(Autocomplete) + fix bag

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
 
         getPlacesData(type, bounds.sw, bounds.ne)
             .then((data) => {
-                setPlaces(data)
+                setPlaces(data.filter((place) => place.name && place.num_reviews > 0))
                 setFilteredPlaces([])
                 setIsLoading(false)
             })
@@ -45,7 +45,7 @@ const App = () => {
     return (
         <>
             <CssBaseline/>
-            <Header/>
+            <Header setCoordinates={setCoordinates}/>
             <Grid container spacing={3} style={{width: '100%'}}>
                 <Grid item xs={12} md={4}>
                     <List
